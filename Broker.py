6dc07be693dfa865e654b7acfa536a532b3f4124
cloareq <quentin.cloarec@epitech.eu>
2015-02-27 09:55:18 +0100

    udptunnel + working client and broker

--- Broker.py
+++ Broker.py
@@ -1,99 +1,159 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
 from thread import start_new_thread
 import hashlib
 import sys
 import socket
 
 SIZE_MSG = 1024
+"""
 
+Broker is a server accessible from the outside. It initiates the connection
+with several clients to make a direct connection between them
+It uses UDP sockets to send and receive DataGram.
+A new thread is open each time a new client sends a message
+
+
+"""
 class Broker():
 
     def __init__(self, ip, port):
+        """ 
+
+        init socket parameters with SO_REUSEADDR to be
+        able to reuse the same address with several clients 
+        
+        """
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self.sock.bind((ip, port))
         self.clients_list = []
         self.socklist = []
         self.client_connection_list = []
 
     def add_client(self, username, password):
+        """ 
+
+        Add a client to the database. Client are defined by a username and
+        a password
+
+        """
+        # delete client if already in the database
         self.delete_client(username)
+        # crypt password
         cid = username + ':' + hashlib.sha1(str(password)).hexdigest() + '\n'
         try:
-            with open('clients.txt', 'a') as file:
+            with open('client_database', 'a') as file:
+                # write new client infos in the database
                 file.write(cid)
         except:
-            f = open('clients.txt', 'w+')
+            f = open('client_database', 'w+')
             f.close()
             self.add_client(username, password)
         
     def delete_client(self, username):
+        """ Delete a client from the database """
         try:
-            f = open("clients.txt", "r+")
+            f = open("client_database", "r+")
         except:
-            f = open("clients.txt", "w+")
+            f = open("client_database", "w+")
         lines = f.readlines()
         f.close()
-        f = open("clients.txt", "w")
+        f = open("client_database", "w")
         for line in lines:
+            # try to find "username" in the database file
             if line[:len(username)] != username:
                 f.write(line)
         f.close()
 
+
+
     def check_registered(self, userinfo):
-        f = open("clients.txt", "r")
+        """ 
+
+        Verify that a user is corresponding 
+        to someone in the database 
+
+        """
+        f = open("client_database", "r")
         flist = f.readlines()
         f.close()
         found = False
         for line in flist:
             if str(userinfo) in line:
+                # user found
                 found = True
                 return True
         if not found:
             return False
 
     def init_socket(self, username, ipclient):
+        """
+
+        initiate a new socket to be able to talk 
+        to a specific client
+
+        """
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind(('127.0.0.1', 0))
+        #bind a free random port
+        s.bind(('', 0))
         port = s.getsockname()[1]
         self.socklist.append((username, s, port, ipclient))
         return s, port
 
-    def catch_connected_clients(self, ip_client, socket_client):
+    def send_connected_clients(self, ip_client, socket_client):
+        """ send all the connected clients """
+
         for client in self.clients_list:
             socket_client.sendto(str(client), ip_client)
         socket_client.sendto("done", ip_client)
 
-    def connection_to_peer(self, ip_client, socket_client, username):
+    def client_to_peer(self, ip_client, socket_client, username):
+        """
+
+        initiate the connection between two clients by
+        sending public ip and port to each of them
+
+        """
         msg, ip = socket_client.recvfrom(1024)
         peer_username = msg
         ip_peer = None
         for client in self.client_connection_list:
             if client[0] == peer_username:
+                #send ip and port to the asking client
                 ip_peer = client[1]
                 socket_client.sendto(ip_peer[0], ip_client)
                 socket_client.sendto(str(ip_peer[1]), ip_client)
-
+                #send a connection request to the peer
                 client[2].sendto("connection request", ip_peer)
                 client[2].sendto(ip_client[0], ip_peer)
                 client[2].sendto(str(ip_client[1]), ip_peer)
 
         if ip_peer == None:
-            socket_client.sendto("User not connnected", ip_client)
+            socket_client.sendto("User not connected", ip_client)
 
 
 
-    def talk_to_client(self, ip_client, socket_client, username):
+    def client_thread(self, ip_client, socket_client, username):
+        """ Listen for a request from a specific client """
+
         while True:
             msg, ip = socket_client.recvfrom(1024)
             if msg == "connected clients":
-                self.catch_connected_clients(ip_client, socket_client)
+                self.send_connected_clients(ip_client, socket_client)
             if msg == "connection socket" :
                 self.client_connection_list.append((username, ip, socket_client))
             if msg.find("request connection") != -1:
-                self.connection_to_peer(ip_client, socket_client, username)
+                self.client_to_peer(ip_client, socket_client, username)
 
-    def listen_clients(self):
+    def start(self):
+        """ 
+        
+        Start the Broker and make it able to wait for clients
+        to connect
+        
+        """
         while True:
             userinfo, ipclient = self.sock.recvfrom(1024)
             if not self.check_registered(userinfo):
@@ -104,11 +164,8 @@ class Broker():
             s, port = self.init_socket(username, ipclient)
             self.clients_list.append(username)
             self.sock.sendto(str(port), ipclient)
-            start_new_thread(self.talk_to_client, (ipclient, s, username))
+            start_new_thread(self.client_thread, (ipclient, s, username))
    
 
-b = Broker('127.0.0.1', 4242)
-b.add_client("quentin", "123")
-b.add_client("bob", "123")
-b.add_client("toto", "123")
-b.listen_clients()
+b = Broker('', 4242)
+b.start()
--- Client.py
+++ Client.py
@@ -1,94 +1,169 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
 import socket
 import hashlib
 from getpass import getpass
 import sys
 from thread import start_new_thread
+from subprocess import call
+
+SIZE_MSG = 1024
+
+"""  
+	A client is able to communicate with the outside and connect
+	to the Broker. It can request a connection with another 
+	conneted client and make a direct connection using 
+	the UDP Hole Punching method.
+	It can be behind firewall/nat.
+
+ """
 
 class Client():
-	""" TODO Docstring client """
 	def __init__(self, broker_ip, broker_port, username, passwd):
+		""" 
+		
+		Initialize all parameters 
+		Socket are initiate with SO_REUSEADDR option to be able
+		to use UDP Hole Punching to send DataGrams to a peer and 
+		set up an UDP Tunnel
+
+		"""
+
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 		self.username = username
 		self.passwd = passwd
-		self.broker_ip = broker_ip
-		self.broker_port = broker_port
+		self.broker_info = (broker_ip, broker_port)
 		self.clients = []
-		self.tmp_sock = None
-		self.ip_peer = None
-		self.port_peer = None
+		self.tmp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.tmp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+		self.peer_info = None
+		self.peer = None
 
 	def identification(self):
+		""" 
+
+		identification to the Broker by sending username and password
+
+
+		"""
+		
+		# Crypt password
 		self.passwd = hashlib.sha1(str(self.passwd)).hexdigest()
 		id_string = self.username + ':' + self.passwd
-		self.sock.sendto(str(id_string), (self.broker_ip, self.broker_port))
-		msg, broker = self.sock.recvfrom(1024)
-		port, broker = self.sock.recvfrom(1024)
+		# Send username and password to the Broker
+		self.sock.sendto(str(id_string), self.broker_info)
+		msg, broker = self.sock.recvfrom(SIZE_MSG)
+		if msg.find("Bad Username or Password") != -1:
+			return -1
+		# Client is correctly identify
+		port, broker = self.sock.recvfrom(SIZE_MSG)
 		self.broker_port = int(port)
+		# start a new thread
 		start_new_thread(self.wait_connection, ())
 
-	def connection_request(self):
-		self.ip_peer, b = self.tmp_sock.recvfrom(1024)
-		print self.ip_peer
-		self.port_peer, b = self.tmp_sock.recvfrom(1024)
-		print self.port_peer
-		self.tmp_sock.sendto("I'm your peer", (self.ip_peer, int(self.port_peer)))
+        def connection_request(self):
+        	# get info from the peer who wants to connect
+            ip_peer, b = self.tmp_sock.recvfrom(SIZE_MSG)
+            port_peer, b = self.tmp_sock.recvfrom(SIZE_MSG)
+            self.peer_info = (ip_peer, int(port_peer))
+            # punch a hole in the NAT
+            self.tmp_sock.sendto("I'm your peer", self.peer_info)
+            # receive a response
+            msg, ip = self.tmp_sock.recvfrom(SIZE_MSG)
+            self.peer_info = ip
+            #get own public ip                                                           
+            myip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0],\
+            	s.close()) for s in [socket.socket(socket.AF_INET,\
+            	socket.SOCK_DGRAM)]][0][1]
+
+            exec_line = "./udptunnelpatch/udptunnel -v -v -v -s " + \
+            str(myip) + " " + str(self.tmp_sock.getsockname()[1])
+            # Udp Tunnel serv mode
+            call([exec_line], shell=True,)
 
 
 	def wait_connection(self):
-		self.tmp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.tmp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self.tmp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-		self.tmp_sock.sendto("connection socket", (self.broker_ip, self.broker_port))
+		# send to the broker the socket able to receive connection from peer
+		self.tmp_sock.sendto("connection socket", self.broker_info)
 
 		while True:
-			msg, ip = self.tmp_sock.recvfrom(1024)
+			msg, ip = self.tmp_sock.recvfrom(SIZE_MSG)
 			if msg == "connection request":
+				# someone ask for a connection
 				self.connection_request()
 
 
-	def connected_clients(self):
-		self.sock.sendto("connected clients", (self.broker_ip, self.broker_port))
+	def get_connected_clients(self):
+		""" get clients username connected to the Broker """
+
+		self.sock.sendto("connected clients", self.broker_info)
 		client = ""
 		while client != "done":
-			client, b = self.sock.recvfrom(1024)
+			client, b = self.sock.recvfrom(SIZE_MSG)
 			if client != "done":
 				self.clients.append(client)
 
-	def connect_to_client(self, username):
+	def connect_to_client(self, username, service_port):
+		""" 
+
+		Ask for a client to make a direct connection and initiate
+		an UDP Tunnel on the service_port
+		
+
+		"""
+
+		# check if client's username is in the connected client's list
 		c = False
 		for client in self.clients:
 			if client == username:
 				c = True
 		if not c:
 			return -1
-		self.sock.sendto("request connection", (self.broker_ip, self.broker_port))
-		self.sock.sendto(username, (self.broker_ip, self.broker_port))
-		msg, b = self.sock.recvfrom(1024)
+		# send the client's username to the broker
+		self.sock.sendto("request connection", self.broker_info)
+		self.sock.sendto(username, self.broker_info)
+
+		# check if the client is connected to the broker
+		msg, b = self.sock.recvfrom(SIZE_MSG)
 		if msg == "User not connected":
-			print msg
 			return -1
+		
+		# get public ip and port of the client
 		self.ip_peer = msg
-		self.port_peer, b = self.sock.recvfrom(1024)
+		self.port_peer, b = self.sock.recvfrom(SIZE_MSG)
+		# make a hole in the NAT
 		self.sock.sendto("peer", (self.ip_peer, int(self.port_peer)))
-		msg, peer = self.sock.recvfrom(1024)
-		print msg
-		print peer
+		# receive a direct response from the client
+		msg, peer = self.sock.recvfrom(SIZE_MSG)
+		self.peer = peer
+        self.sock.sendto("peer", self.peer)
+        # get own public ip
+		myip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close())	\
+		for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+		
+		# initiate the UDP tunnel on the service port as a client
+
+		exec_line = "./udptunnelpatch/udptunnel -c " + str(myip) + \
+		" 4444 " + str(self.peer[0]) + " " + str(self.peer[1])\
+		+ " 127.0.0.1 " + service_port + " " +\
+		str(self.sock.getsockname()[1])
+        
+        # exec UDPTunnel        
+		call([exec_line], shell=True,)
 
 
 def main():
 	if sys.argv[5] == "1":
-		print 1
 		c = Client(sys.argv[1], int(sys.argv[2]), sys.argv[3], sys.argv[4])
 		c.identification()
-		c.connected_clients()
+		c.get_connected_clients()
 	else:
-		print 2
 		c = Client(sys.argv[1], int(sys.argv[2]), sys.argv[3], sys.argv[4])
 		c.identification()
-		c.connected_clients()
+		c.get_connected_clients()
 		c.connect_to_client("bob")
 	while True:
 		pass
